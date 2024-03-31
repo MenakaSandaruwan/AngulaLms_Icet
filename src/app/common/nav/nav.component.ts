3614062737e86676a62fce785810f029766d5d84
karunarathnawmwgms <122153208+karunarathnawmwgms@users.noreply.github.com>
2024-03-31 16:10:39 +0530

    frist

---
+++ src/app/common/nav/nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [NavComponent],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+
+}
